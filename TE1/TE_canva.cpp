e7e17fde25f718935e176d67c8340ab5dc14f579
BernardLhermite <stephane.marengo@hotmail.com>
2022-05-11 21:28:53 +0200

    Formattage

--- TE1/TE_canva.cpp
+++ TE1/TE_canva.cpp
@@ -1,70 +1,67 @@
 #include <iostream>
 #include <string>
 #include <memory>
+
 using namespace std;
 
-class Station
-{
-  const string _name;
+class Station {
+   const string _name;
 
-  Station(const Station& o) = delete;
-  void operator=(const Station& o) = delete;
+   Station(const Station& o) = delete;
+
+   void operator=(const Station& o) = delete;
 
 public:
-  Station(string name) : _name(name) {
-  }
+   Station(string name) : _name(name) {}
 
-  virtual string toString() const {
-    return _name;
-  }
+   virtual string toString() const {
+      return _name;
+   }
 
-  virtual ~Station() {
-  }
+   virtual ~Station() {}
 };
 
-class Jedi
-{
-  string _name;
+class Jedi {
+   string _name;
 public:
 
-  Jedi(string name): _name(name) {}
-  
-  ~Jedi() {
-    cout << _name << " vanishes in the Force..." << endl;
-  }
+   Jedi(string name) : _name(name) {}
+
+   ~Jedi() {
+      cout << _name << " vanishes in the Force..." << endl;
+   }
 };
 
 
-int main() 
-{
-  // Exercice 1
-  
-  const MainStation
-    s42("Grand Central", "Train/Subway connections"),
-    s23("14 Street", "Subway connections"),
-    sTS("Times Square", "Subway connections");
+int main() {
+   // Exercice 1
 
-  const Station
-    s33("33 Street"),
-    s28("28 Street"),
-    s05("5th Avenue");
+   const MainStation
+      s42("Grand Central", "Train/Subway connections"),
+      s23("14 Street", "Subway connections"),
+      sTS("Times Square", "Subway connections");
 
-  Line
-    line6(6, { &s42, &s33, &s28, &s23 }),
-    line7(7, { &s42, &s05, &sTS });
+   const Station
+      s33("33 Street"),
+      s28("28 Street"),
+      s05("5th Avenue");
 
-  cout << line6.route(false) << endl;
-  cout << line6.route(true) << endl;
-  cout << line7.route(false) << endl;
+   Line
+      line6(6, {&s42, &s33, &s28, &s23}),
+      line7(7, {&s42, &s05, &sTS});
 
-  // Exercice 2
-  
-  // définition de Luke
-  // définition de Rey
-  
-  cout << luke->toString() << endl;
-  cout << rey->toString() << endl;
+   cout << line6.route(false) << endl;
+   cout << line6.route(true) << endl;
+   cout << line7.route(false) << endl;
 
-  // Mort de Luke
-  cout << rey->toString() << endl;
+   // Exercice 2
+
+   // définition de Luke
+   // définition de Rey
+
+   cout << luke->toString() << endl;
+   cout << rey->toString() << endl;
+
+   // Mort de Luke
+   cout << rey->toString() << endl;
 }
