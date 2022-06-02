c7b89b4d4efd79a06024824724b9dfa71ca86d8d
BernardLhermite <stephane.marengo@hotmail.com>
2022-06-02 14:39:33 +0200

    Ex5

---
+++ Java/src/Chap2Ex5.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class Words {
+    public static int vowels(String w) {
+        return w.length() - w.toLowerCase().replaceAll("[aâàäæeéêèëiîïoôœöuûùüyÿ]", "").length();
+    }
+}
+
+public class Chap2Ex5 {
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList(Pays.get());
+        Collections.sort(words, Comparator.comparing(Words::vowels).thenComparing(String::compareTo));
+        System.out.println(words);
+    }
+}
